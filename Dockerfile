b7c62628b397ed75f8811a20f677024c92fd4fd6
mannapushpendu <mannapushpendu@gmail.com>
2020-05-03 18:58:16 +0000

    added postgres get and post methods

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 # Use the official lightweight Python image.
 # https://hub.docker.com/_/python
-FROM python:3.7-slim
+FROM python:3.7-alpine
 
 EXPOSE 8080
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
+RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev
 COPY requirements.txt /usr/src/app/
 RUN pip install --no-cache-dir -r requirements.txt
 
--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 import tornado.web
 import tornado.ioloop
 import json
+import psycopg2
+import os
+
 class mainRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
@@ -16,11 +19,39 @@ class listRequestHandler(tornado.web.RequestHandler):
         fh.write(f"{fruit}\n")
         fh.close()
         self.write(json.dumps({"message": "Fruit added successfully."}))
+
+class employeeRequestHandler(tornado.web.RequestHandler):
+    def initialize(self, con):
+        self.con = con
+        self.cur = con.cursor()
+
+    def get(self):
+        self.cur.execute("select * from employee")
+        rows = self.cur.fetchall()
+        self.write(json.dumps(rows))
+
+    def post(self):
+        newEmp = self.get_argument("emp")
+        self.cur.execute("insert into employee (name) values (%s)", (newEmp,))
+        self.con.commit()
+        self.write({"message":"Employee added successfully"})
+
+
     
 if __name__ == "__main__":
+    con = psycopg2.connect(
+        host = "10.9.176.23",
+        database =  "postgres",
+        user = "postgres",
+        password = os.environ['POSTGRES_PASSWORD']
+        )
+
     app = tornado.web.Application([
         (r"/", mainRequestHandler),
-        (r"/list", listRequestHandler)
+        (r"/list", listRequestHandler),
+        (r"/employee", employeeRequestHandler, {
+            'con': con
+        })
     ])
     
     port = 8080
